1835e410a7e35efff6e0e72173f6d3a37c6459b3
peluza <edisonisaza@gmail.com>
2020-04-30 15:46:55 -0500

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+    a = len(argv) -1
+    j = 1
+    if a == 0:
+        print("{:d}: argument.".format(a))
+    else:
+        print("{:d}: argument:".format(a))
+        for i in range(1, len(argv)):
+            print("{:d}:".format(j), argv[i])
+            j += 1
