d3c5bc7d843c4fbdd19607ea562fd9e9b5a9529c
peluza <edisonisaza@gmail.com>
2020-05-05 15:22:37 -0500

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for j in my_string:
-        for i in range(len(j)):
-            if ord(j[i]) == 67 or ord(j[i]) == 99:
-                new_string = my_string[:i] + my_string[i + 1:]
-            else:
-                new_string = my_string
-            return(new_string)
+    new_string = ""
+    for i in my_string:
+        if ord(i) != 99 and ord(i) != 67:
+         new_string += i
+    return(new_string)
