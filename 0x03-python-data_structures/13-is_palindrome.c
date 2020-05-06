49f049a8cb36ef54a09c49e998cc51e5f61a9aa1
peluza <edisonisaza@gmail.com>
2020-05-05 22:06:03 -0500

    13-is_palindrome.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+listint_t *palindrome_rec(listint_t *tail, listint_t *head);
+/**
+ *palindrome_rec - this function evaluates if singly linked list is palindrome
+ *@head: head of the singly linked list
+ *@tail: tail of the end of the  linked list
+ *Return: 0 if it is not palidrome or 1 if it is palindrome
+ */
+listint_t *palindrome_rec(listint_t *tail, listint_t *head)
+{
+	if (tail->next != 0)
+		head = palindrome_rec(tail->next, head);
+	if (head != 0)
+	{
+		if (head->next == 0)
+			return (tail);
+		if (head->n == tail->n)
+			return (head->next);
+	}
+	return (0);
+}
+
+/**
+ *is_palindrome - this function evaluates if singly linked list is palindrome
+ *@head: head of the singly linked list
+ *Return: 0 if it is not palidrome or 1 if it is palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	if (head[0] == 0)
+		return (1);
+	if (palindrome_rec(head[0], head[0]) == 0)
+		return (0);
+	return (1);
+}
