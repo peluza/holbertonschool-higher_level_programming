3c117286b602272b290fde94076759b44c082964
peluza <edisonisaza@gmail.com>
2020-05-07 17:41:29 -0500

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if not a_dictionary:
-        return (None)
-    new = sorted(a_dictionary.items())
-    x, y = new[-1]
-    return x
+        return None
+    tmp = None
+    for i, j in a_dictionary.items():
+        if not tmp or j > a_dictionary[tmp]:
+            tmp = i
+    return tmp
