5f7ddabc4ce63d34613346af5e95abddad62b128
peluza <edisonisaza@gmail.com>
2020-05-07 17:20:43 -0500

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return (None)
-    new = sorted(a_dictionary.items(), key=lambda xy: (xy))
+    new = sorted(a_dictionary.items())
     x, y = new[-1]
     return x
