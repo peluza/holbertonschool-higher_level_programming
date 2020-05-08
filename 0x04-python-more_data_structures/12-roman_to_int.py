ba1ed8af97d7202467ed68b1abd44f42ef3b2117
peluza <edisonisaza@gmail.com>
2020-05-07 20:45:33 -0500

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return None
+    roman = {'I': 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+    num = 0
+    for a, b in zip(roman_string, roman_string[1:]):
+        if roman[a] < roman[b]:
+            num -= roman[a]
+        else:
+            num += roman[a]
+    num += roman[roman_string[-1]]
+    return num
