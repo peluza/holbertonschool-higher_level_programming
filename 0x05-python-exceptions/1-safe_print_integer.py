d5bf3fae35da0205ef10dcea550e616d9ccff1c8
peluza <edisonisaza@gmail.com>
2020-05-18 12:11:17 -0500

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     value
     try:
-        print("{:d}\n".format(value), end='')
+        print("{:d}".format(value), end='\n')
         return True
     except:
         return False
