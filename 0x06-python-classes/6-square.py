c715e02863da0d805e572cf9680f972922dfd249
peluza <edisonisaza@gmail.com>
2020-05-19 18:26:41 -0500

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -27,13 +27,6 @@ class Square:
 
     def my_print(self):
         """class Square.my_print"""
-        if (self.__size != 0):
-            print(('\n' * self.__position[1]), end="")
-            for i in range(self.__size):
-                print((" " * self.__position[0]), end='')
-                print ("#" * self.__size)
-            else:
-                print("")
 
     @property
     def size(self):
