5feb96fc1542f731cf0ec9d1ef1824a93e528971
peluza <edisonisaza@gmail.com>
2020-05-19 18:06:50 -0500

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -75,5 +75,3 @@ class Square:
             for i in range(self.__size):
                 print((" " * self.__position[0]), end='')
                 print ("#" * self.__size)
-            else:
-                print("")
