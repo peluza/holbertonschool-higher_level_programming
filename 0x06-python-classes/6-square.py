c651a66ed5d49e4f88e2bfa7e6e09feddcbe7efd
peluza <edisonisaza@gmail.com>
2020-05-19 18:55:50 -0500

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -25,9 +25,6 @@ class Square:
         """class Square.area"""
         return self.__size * self.__size
 
-    def my_print(self):
-        """class Square.my_print"""
-
     @property
     def size(self):
         """class size"""
