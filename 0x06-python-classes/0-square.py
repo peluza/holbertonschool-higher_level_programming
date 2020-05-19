4a595c5a8b9eed939e2b4ed8f485d2ba328d29aa
peluza <edisonisaza@gmail.com>
2020-05-19 16:35:00 -0500

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
+"""class Square"""
+
+
 class Square:
-    def __init__(self):
-        """By default private members are not included.
-
-        Private members are any methods or attributes that start with an
-        underscore and are *not* special. By default they are not included
-        in the output.
-
-        This behavior can be changed such that private members *are* included
-        by changing the following setting in Sphinx's conf.py::
-
-            napoleon_include_private_with_doc = True
-
-        """
-        pass
+    """class Square"""
+    pass
