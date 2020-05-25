7f5af206dcf137db6c45a3c07d9a02350431b6ab
peluza <edisonisaza@gmail.com>
2020-05-25 18:38:13 -0500

    2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -11,23 +11,23 @@ class Rectangle:
             width {int} -- width the rectangle (default: {0})
             height {int} -- height the rectangle (default: {0})
         """
-        self.width = width
-        self.height = height
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
 
     def area(self):
-        """area
-
-        Returns:
-            self -- width * height
-        """
         return self.width * self.height
 
     def perimeter(self):
-        """perimeter
-
-        Returns:
-            self -- 2 * height + 2 * width
-        """
         if self.width == 0 or self.height == 0:
             return 0
         else:
@@ -35,24 +35,10 @@ class Rectangle:
 
     @property
     def width(self):
-        """width (self)
-
-        Returns:
-            self -- width
-        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """width (self,value)
-
-        Arguments:
-            value {int} -- value is at intenger positve
-
-        Raises:
-            TypeError: width must be an integer
-            ValueError: width must be >= 0
-        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -62,24 +48,10 @@ class Rectangle:
 
     @property
     def height(self):
-        """heigth(self)
-
-        Returns:
-            self -- height
-        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """heigth(self, value)
-
-        Arguments:
-            value {int} -- value is the number interger positive
-
-        Raises:
-            TypeError: height must be an integer
-            ValueError: height must be >= 0
-        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
