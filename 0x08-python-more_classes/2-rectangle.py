ddf0bb40dc1598611e025302a1500a938d54544c
peluza <edisonisaza@gmail.com>
2020-05-25 18:47:35 -0500

    2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -19,8 +19,8 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     def area(self):
         """area
