296351b664927929be435029dcc4636727fdf726
peluza <edisonisaza@gmail.com>
2020-05-26 21:06:58 -0500

    test

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,15 +1,3 @@
 #!/usr/nin/python3
-"""19-copy_list
-    """
-
-
 def copy_list(l):
-    """copy_list
-
-    Arguments:
-        l {list} -- the parameter is the list
-
-    Returns:
-        list -- at copy
-    """
     return l[:]
