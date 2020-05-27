f83a540816600f341e3917e291fdef84b3cac5cd
peluza <edisonisaza@gmail.com>
2020-05-26 21:27:42 -0500

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""101-locked_class
+    """
+
+
 class LockedClass:
+    """Lockedclass
+    """
     __slots__ = "first_name"
 
     def __init__(self, first_name=""):
+        """[__init__
+
+        Keyword Arguments:
+            first_name {str} -- first_name (default: {""})
+        """
         self.first_name = first_name
