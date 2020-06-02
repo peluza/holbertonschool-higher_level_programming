a6d3c89437bfa1c08771bf11276f06e93f75ab20
peluza <edisonisaza@gmail.com>
2020-06-02 17:53:35 -0500

    10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""10-square
+    """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square(Rectangle)
+
+    Arguments:
+        Rectangle {int} -- call class Rectangle
+    """
+
+    def __init__(self, size):
+        """__init__(self,size)
+
+        Arguments:
+            size {int} -- the number at size for square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
