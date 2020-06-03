1da1de157b929b3bb5108ef6fd375905969e2e81
peluza <edisonisaza@gmail.com>
2020-06-03 18:16:11 -0500

    10-class_to_json.py

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -4,4 +4,12 @@
 
 
 def class_to_json(obj):
+    """class_to_json(json)
+
+    Arguments:
+        obj {str} -- the value at the dictory
+
+    Returns:
+        dict -- returns the cointains obj at dictory
+    """
     return obj.__dict__
