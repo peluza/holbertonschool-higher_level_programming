a94ab8358e829611bbeaf1a83e5421510c0bad0a
peluza <edisonisaza@gmail.com>
2020-06-03 17:39:06 -0500

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""read_file
+    """
+
+
+def read_file(filename=""):
+    """read_file(filename)
+
+    Keyword Arguments:
+        filename {str} -- name of the file (default: {""})
+    """
+    with open(filename, encoding="utf-8") as my_file:
+        print(my_file.read())
