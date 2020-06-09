a428ad684c8cbb6e21628b2f7d7be34e0dbce481
peluza <edisonisaza@gmail.com>
2020-06-09 12:12:58 -0500

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,7 +43,8 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         new_list = []
-        if list_objs is None:
+        if list_objs is None and len(list_objs) == 0 and type(
+                list_objs) != list:
             with open(
                     cls.__name__ + ".json", mode="w", encoding="utf-8") as f:
                 f.write(new_list)
