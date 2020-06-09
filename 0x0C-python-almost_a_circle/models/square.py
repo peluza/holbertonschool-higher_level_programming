db461ff1875226a163e654468040d99be98cdffa
peluza <edisonisaza@gmail.com>
2020-06-09 11:27:07 -0500

    square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,7 +5,21 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """Square(Rectangle)
+
+    Args:
+        Rectangle (int): the value is inter, id optional
+    """
+
     def __init__(self, size, x=0, y=0, id=None):
+        """__init__(self,size,x,y,id)
+
+        Args:
+            size (int): the size is the intenger
+            x (int): the x is intenger >= 0. Defaults to 0.
+            y (int): the y is intenger >= 0. Defaults to 0.
+            id (optional): the id id type opcional. Defaults to None.
+        """
         super().__init__(size, size, x, y, id)
         self.size = size
 
