390733d045514524338cd2e054e413d2fcaec5a5
peluza <edisonisaza@gmail.com>
2020-07-13 23:31:06 +0000

    100-move_to_utf8.sql

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best
+ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `hbtn_0c_0`.`first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
