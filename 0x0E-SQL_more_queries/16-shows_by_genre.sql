fcd2f83e3a4ff6c4a5118832cb4318477cb51d54
peluza <edisonisaza@gmail.com>
2020-07-15 02:29:31 +0000

    16-shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name ASC;
