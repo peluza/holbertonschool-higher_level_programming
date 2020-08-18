c16295bc71830e9531ac5b7efefe1e50021e3218
peluza <edisonisaza@gmail.com>
2020-08-18 17:55:32 +0000

    3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#URL and displays all HTTP methods
-curl -s -X OPTIONS "$1" -i |  awk '$1=="Allow:" {print $2, $3, $4}'
+#OPTIONS
+curl -s -X OPTIONS "$1" -i | awk '$1=="Allow:" {print $2, $3, $4}'
