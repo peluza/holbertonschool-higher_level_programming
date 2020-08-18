ad6cd46250ea1399a0b3c3055e4d2783986915ac
peluza <edisonisaza@gmail.com>
2020-08-18 21:13:21 +0000

    6-peak.py

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" finds a peak in a list of unsorted integers.
+    """
+
+
+def find_peak(list_of_integers):
+    """find_peak
+
+    Args:
+        list_of_integers (list): list of unsorted integers.
+
+    Returns:
+        list: number peak in a list
+    """
+    if len(list_of_integers) is not 0:
+        list_of_integers.sort()
+        return list_of_integers[-1]
+    else:
+        return None
