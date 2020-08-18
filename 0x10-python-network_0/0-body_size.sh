0282118fff726de544bb570180284eed42533743
peluza <edisonisaza@gmail.com>
2020-08-18 06:20:41 +0000

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #displays the size of the body of the response
-curl -sI klase.tech | head -n 5 | tail -n 1 | awk '{print $2}'
+curl -sI "$1" | head -n 5 | tail -n 1 | awk '{print $2}'
