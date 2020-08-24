244578fc6bf7625dd7f33ade1e64650c86989a87
peluza <edisonisaza@gmail.com>
2020-08-24 23:22:46 +0000

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,13 +8,17 @@ import sys
 
 if __name__ == "__main__":
 
-    user = sys.argv[2]
     rep = sys.argv[1]
+    user = sys.argv[2]
     url = "https://api.github.com/repos/" + user + "/" + rep + "/commits"
     try:
         req = requests.get(url)
         json = req.json()
+        count = 0
         for i in json:
             print("{}: {}".format(i['sha'], i['commit']['author']['name']))
+            count += 1
+            if count == 10:
+                break
     except:
         print("None")
