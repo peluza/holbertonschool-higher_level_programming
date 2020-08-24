3e036ad26b6ceffbdcb4c56c6a599c6c71c7c4ad
peluza <edisonisaza@gmail.com>
2020-08-24 23:15:14 +0000

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,12 +8,11 @@ import sys
 
 if __name__ == "__main__":
 
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    url = 'https://api.github.com/repos/peluza/holbertonschool-higher_level_programming/commits'
-    data = requests.auth.HTTPBasicAuth(user, passwd)
+    user = sys.argv[2]
+    rep = sys.argv[1]
+    url = "https://api.github.com/repos/" + user + "/" + rep + "/commits"
     try:
-        req = requests.get(url, auth=data)
+        req = requests.get(url)
         json = req.json()
         for i in json:
             print("{}: {}".format(i['sha'], i['commit']['author']['name']))
