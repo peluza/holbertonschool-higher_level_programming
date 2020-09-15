5bbdb44b97977017bb22c3a68b8feff0fc912607
peluza <edisonisaza@gmail.com>
2020-09-15 19:12:32 +0000

    4-concat.js

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Value of my argument
+const argv = process.argv.slice(2);
+if (!argv[0]) {
+  console.log('No argument');
+} else {
+  console.log(argv[0] + ' is ' + argv[1]);
+}
