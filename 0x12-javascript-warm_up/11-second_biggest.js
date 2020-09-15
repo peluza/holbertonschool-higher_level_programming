a6f4d7864f4cdd8a841229fffeef36f6bbeebd5c
peluza <edisonisaza@gmail.com>
2020-09-15 21:23:14 +0000

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 // Second biggest!
 const argv = process.argv.slice(2);
-const number = parseInt(argv[0], 10);
-if (!number) {
+const number1 = parseInt(argv[0], 10);
+const number2 = parseInt(argv[1], 10);
+if (!number1) {
+  console.log(0);
+} else if (number1 === 1 && !number2) {
   console.log(0);
 } else {
   argv.sort();
