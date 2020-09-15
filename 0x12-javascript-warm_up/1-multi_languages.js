ec24991376520bc360efda316eae34b738c9f8da
peluza <edisonisaza@gmail.com>
2020-09-15 17:10:25 +0000

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// 3 languages
+const Javascript = 'Javascript is amazing';
+const Python = 'Python is cool';
+const C = 'C is fun';
+console.log(C);
+console.log(Python);
+console.log(Javascript);
