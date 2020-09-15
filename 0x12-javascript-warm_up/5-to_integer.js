2a030fc90dc6b6377393b4ad151299fdce88f21b
peluza <edisonisaza@gmail.com>
2020-09-15 19:27:54 +0000

    5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Value of my argument
+const argv = process.argv.slice(2);
+const number = parseInt(argv[0], 10);
+if (!number) {
+  console.log('Not a number');
+} else {
+  console.log(number);
+}
