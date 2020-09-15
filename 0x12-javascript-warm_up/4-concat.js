880d81fd284bf7c6ca32e5e5b56ec704131466db
peluza <edisonisaza@gmail.com>
2020-09-15 19:16:44 +0000

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
 // Create a sentence
 const argv = process.argv.slice(2);
-if (!argv[0]) {
-  console.log('No argument');
-} else {
-  console.log(argv[0] + ' is ' + argv[1]);
-}
+console.log(argv[0] + ' is ' + argv[1]);
