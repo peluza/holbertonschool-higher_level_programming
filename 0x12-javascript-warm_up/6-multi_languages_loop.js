6cc3c415ed12659bfc7f05b9cd0302125705ce64
peluza <edisonisaza@gmail.com>
2020-09-15 19:42:54 +0000

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Loop to languages
+const Javascript = 'Javascript is amazing';
+const Python = 'Python is cool';
+const C = 'C is fun';
+const arr = [C, Python, Javascript];
+var i;
+for (i = 0; i < 3; i++) {
+  console.log(arr[i]);
+}
