62ff9428f780edfc76051f0d24080e33027d5131
peluza <edisonisaza@gmail.com>
2020-09-21 22:26:40 +0000

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 console.log(list);
-const map1 = list.map(x => x * list.indexOf(x));
+const map1 = list.map((value, index) => { return value * index; });
 console.log(map1);
