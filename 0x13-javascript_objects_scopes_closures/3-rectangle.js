e9a90d5674c398fa806c84359857906522c02461
peluza <edisonisaza@gmail.com>
2020-09-21 19:30:16 +0000

    3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i;
+    const x = 'X';
+    for (i = 0; i < this.height; i++) {
+      console.log(x.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
