febba535468a89890b53c148742d706a64c2530f
peluza <edisonisaza@gmail.com>
2020-09-21 23:31:11 +0000

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-console.log(dict);
 const newdict = {};
 for (const [key, value] of Object.entries(dict)) {
   if (newdict[value] === undefined) {
