5babf76c45f08fe07ca56ecd6b1f1a657daba2d7
peluza <edisonisaza@gmail.com>
2020-09-21 20:27:07 +0000

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,22 +2,22 @@
 const Pared = require('./5-square');
 
 class Square extends Pared {
-    constructor(size) {
-        super(size, size);
-        this.size = size;
-    }
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
 
-    charPrint(c) {
-        if (c) {
-            let i;
-            const word = 'C';
-            for (i = 0; i < this.size; i++) {
-                console.log(word.repeat(this.size));
-            }
-        } else {
-            this.print();
-        }
+  charPrint (c) {
+    if (c) {
+      let i;
+      const word = 'C';
+      for (i = 0; i < this.size; i++) {
+        console.log(word.repeat(this.size));
+      }
+    } else {
+      this.print();
     }
+  }
 }
 
 module.exports = Square;
