9f2041bd6beec4b9c869b03634281851dd099bd2
peluza <edisonisaza@gmail.com>
2020-09-22 17:11:45 +0000

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-/*  script that concats 2 files */
 const args = process.argv;
-const file1 = args[2];
 const fs = require('fs');
-fs.readFile(file1, 'utf-8', function (err, res) {
+fs.readFile(args[2], 'utf-8', function (err, res) {
   if (err) {
     return console.error(err);
   }
