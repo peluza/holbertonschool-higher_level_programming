c2f60c5cc6651aa9133ea328c2942e79e10b723f
peluza <edisonisaza@gmail.com>
2020-09-22 17:19:50 +0000

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const args = process.argv.slice(2, 3);
 fs.readFile(args[0], 'utf-8', function (err, res) {
   if (err) {
-    return console.error(err);
+    return console.log(err);
   }
   console.log(res);
 });
