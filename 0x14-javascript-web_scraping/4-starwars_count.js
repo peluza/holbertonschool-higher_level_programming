a859ae2c6fe812df32ef3cf16a74baa388678548
peluza <edisonisaza@gmail.com>
2020-09-22 19:42:03 +0000

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,16 +2,16 @@
 const args = process.argv;
 const url = args[2];
 const request = require('request');
+let newdict;
+let i;
+let count = 0;
+let j;
 request(url, function (error, response, body) {
   if (error) {
     return console.error(error);
   }
-  let newdict = {};
   newdict = JSON.parse(body).results;
-  let i;
-  let count = 0;
   for (i = 0; i < newdict.length; i++) {
-    let j;
     for (j = 0; j < newdict[i].characters.length; j++) {
       if (newdict[i].characters[j] === 'https://swapi-api.hbtn.io/api/people/18/') {
         count = count + 1;
